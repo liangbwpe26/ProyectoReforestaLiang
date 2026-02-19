55e7e68cec402fd0b7ad94a7b7485fe71185cbd0
franuh23 <franuh23@gmail.com>
2026-02-19 09:57:50 +0100

    Creacion modelos con sus migraciones y recursos

---
+++ src/app/Http/Controllers/EventosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Eventos;
+use Illuminate\Http\Request;
+
+class EventosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Eventos $eventos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Eventos $eventos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Eventos $eventos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Eventos $eventos)
+    {
+        //
+    }
+}
